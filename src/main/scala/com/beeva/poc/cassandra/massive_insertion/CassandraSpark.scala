757d3e1ac415535ee1824ba996c8ad1be3b89e9a
pablo jimenez <pablo.jimenez@beeva.com>
2015-06-23 11:29:55 +0200

    cleaning spark reader

--- src/main/scala/com/beeva/poc/cassandra/massive_insertion/CassandraSpark.scala
+++ src/main/scala/com/beeva/poc/cassandra/massive_insertion/CassandraSpark.scala
@@ -8,42 +8,31 @@ import java.text.SimpleDateFormat
 //lectura de datos con spark y calculo de maximos mínimos y media de temperaturas
 object CassandraSpark extends App {
   val format = new SimpleDateFormat("HH:mm:ss.SSSZ")
-  case class EventTemperature(weatherstation_id: String, date: String, event_time:Long, temperature:String) {
-    override def toString = printf("id:%s , date:%s , time:%s , temperature:%s event", weatherstation_id, date, format.format(event_time), temperature).toString
-  }
   
   val conf = new SparkConf(true).set("spark.cassandra.connection.host", "127.0.0.1")
-
   val sc = new SparkContext("local", "testingCassy", conf)
-  val rdd = sc.parallelize(1 to 10)
-
-  
-  var tableN = sc.cassandraTable("test", "temperature_by_day")
-  println("first " +tableN.first())
   
   var ini = System.currentTimeMillis()
-  val table = sc.cassandraTable("test", "temperature_by_day").select("weatherstation_id","date", "event_time", "temperature").where("weatherstation_id = ? and date=?", "Station-9", "2015-10-13")
-  println("query "+table.first()+ " "+(System.currentTimeMillis()-ini)+" mls")
+  val date = RandomObject.getDate()
+  val table = sc.cassandraTable("test", "temperature_by_day").select("weatherstation_id","date", "event_time", "temperature").where("weatherstation_id = ? and date=?", RandomObject.getRandomID(), RandomObject.getDateStr(date))
+  println("First Row: "+table.first()+ " "+(System.currentTimeMillis()-ini)+" mls")
   
   
   ini = System.currentTimeMillis()
   val min = table.map { x => (x.get[String]("temperature").toInt, 1) }.reduce((a,b) => {
     if(a._1<b._1) a else b}
   )
-  println("min:"+min)
-  println("min "+(System.currentTimeMillis()-ini)+" mls")
-  
- 
+  println("Min:"+min._1+" "+(System.currentTimeMillis()-ini)+" mls")
+   
   ini = System.currentTimeMillis()
   val max = table.map { x => (x.get[String]("temperature").toInt, 1) }.reduce((a,b) => {
     if(a._1>b._1) a else b}
   )
-  println("max:"+max)
-  println("max "+(System.currentTimeMillis()-ini)+" mls")
+  println("Max:"+max._1+" "+(System.currentTimeMillis()-ini)+" mls")
   
   ini = System.currentTimeMillis()
-  val go = table.map { x => (x.get[String]("temperature").toInt, 1) }.reduce((a,b) => ((a._1 + b._1), (a._2 + b._2)))
-  println("go "+(System.currentTimeMillis()-ini)+" mls avg:"+ go._1/go._2)
+  val avg = table.map { x => (x.get[String]("temperature").toInt, 1) }.reduce((a,b) => ((a._1 + b._1), (a._2 + b._2)))
+  println("Average: "+ avg._1/avg._2 + " "+(System.currentTimeMillis()-ini)+" mls")
   
-  println("rows count: "+table.count())
+  println("Rows count: "+table.count())
 }
